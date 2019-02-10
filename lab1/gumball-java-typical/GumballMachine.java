e0b091fe15d63f624bdbc5ec51aa8c2e0385395a
Viraj Upadhyay <u.viraj1995@gmail.com>
2019-02-09 20:50:57 -0800

    Code working for question 1 : Gumball-java-typical

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -4,31 +4,48 @@ public class GumballMachine
 
     private int num_gumballs;
     private boolean has_quarter;
+    private int gumballPrice;
+    private int total;
+    private String gumballType;
 
-    public GumballMachine( int size )
+    public GumballMachine( int size, int price, String type )
     {
         // initialise instance variables
         this.num_gumballs = size;
         this.has_quarter = false;
+        this.gumballType = type;
+        this.gumballPrice = price;
+        this.total = 0;
     }
 
     public void insertQuarter(int coin)
     {
-        if ( coin == 25 )
+        if (( coin == 25 ) && (this.gumballPrice == 25) && (this.gumballType.equals("Quarters")))
+        {
             this.has_quarter = true ;
-        else 
-            this.has_quarter = false ;
+        }
+        else if(coin == 25 && gumballPrice == 50 && gumballType.equals("Quarters"))
+        {
+            total = total + coin;
+        }
+        else if (gumballPrice == 50 && !gumballType.equals("Quarters"))
+        {
+            total = total + coin;
+        }
+        else {
+            System.out.println("Invalid");
+        }
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.has_quarter || (this.total >=this.gumballPrice ) )
     	{
     		if ( this.num_gumballs > 0 )
     		{
     			this.num_gumballs-- ;
     			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			System.out.println( "Thanks.  Gumball Ejected!" ) ;
     		}
     		else
     		{
@@ -37,7 +54,7 @@ public class GumballMachine
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+    		System.out.println( "Please check the coin you inserted." ) ;
     	}        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,20 +3,53 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
+		GumballMachine gumballMachine = new GumballMachine(5, 25, "Quarters");
 
-		System.out.println(gumballMachine);
-
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+		System.out.println("");
+		System.out.println("Gumball Machine 1");
 
 		gumballMachine.insertQuarter( 25 );
 		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
+		gumballMachine.insertQuarter ( 5 );
 		gumballMachine.turnCrank();
+		
 
-		System.out.println(gumballMachine);
+		System.out.println("");
+		GumballMachine gumballMachine1 = new GumballMachine(5, 50, "Quarters");
+		System.out.println("Gumball Machine 2");
+
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertQuarter ( 25 );
+		gumballMachine1.insertQuarter ( 25 );
+		gumballMachine1.turnCrank();
+
+		System.out.println("");
+		GumballMachine gumballMachine2 = new GumballMachine(1, 50, "Any");
+		System.out.println("Gumball Machine 3");
+		
+		gumballMachine2.insertQuarter( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter( 5 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter ( 10 );
+		gumballMachine2.insertQuarter ( 10 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter ( 25 );
+		gumballMachine2.insertQuarter ( 5 );
+		gumballMachine2.insertQuarter ( 10 );
+		gumballMachine2.insertQuarter ( 10 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertQuarter ( 25 );
+		gumballMachine2.insertQuarter ( 25 );
+		gumballMachine2.turnCrank();
+		
+		System.out.println("");
+		System.out.println("");
+		
+		
+		
 	}
 }
